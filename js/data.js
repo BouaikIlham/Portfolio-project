bbcb73d97c259bfd69ca392b163a1c0fab46f343
ilham bouaik <ibouaik@gmail.com>
2022-04-13 16:11:27 +0100

    add data

---
+++ js/data.js
@@ -0,0 +1,71 @@
+const data = [
+  {
+    id: 1,
+    image: 'images/SnapshootPortfolio.jpg',
+    name: 'Tonic',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius quae optio et quas, quibusdam quis excepturi omnis? Quos, officia accusamus. Aperiam, molestiae elit. atque maiores aperiam.    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita ex quam officia ea dolor. Ea quasi in id dolorum ',
+    detail:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: ' https://bouaikilham.github.io/Portfolio-project/',
+    sourceCode: 'https://github.com/BouaikIlham/Portfolio-project',
+  },
+  {
+    id: 2,
+    image: 'images/SnapshootPortfolio.png',
+    name: 'Multi-Post Stories',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: ' https://bouaikilham.github.io/Portfolio-project/',
+    sourceCode: 'https://github.com/BouaikIlham/Portfolio-project',
+  },
+  {
+    id: 3,
+    image: 'images/portfolio.jpg',
+    name: 'Tonic',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+      'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: ' https://bouaikilham.github.io/Portfolio-project/',
+    sourceCode: 'https://github.com/BouaikIlham/Portfolio-project',
+  },
+  {
+    id: 4,
+    image: 'images/portfolio1.jpg',
+    name: 'Multi-Post Stories',
+    description: {
+      type: 'CANOPY',
+      interface: 'Backend Dev',
+      year: 2015,
+    },
+    fullDesciption:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    detail:
+      'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    technologies: ['Html', 'Css', 'Javascript', 'github', 'Bootstrap'],
+    liveDemo: ' https://bouaikilham.github.io/Portfolio-project/',
+    sourceCode: 'https://github.com/BouaikIlham/Portfolio-project',
+  },
+];
+export default data;
